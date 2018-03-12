cdc0e6bb1a6ce7e692c0c0c646f4f155542861e4
Andrewwss <AndriiSlichenko@gmail.com>
2018-03-12 15:34:24 +0200

    Revert "Delete README.md"
    This reverts commit ad8dcd15555895cef614e5dc91eca84982197689.
    
    # Conflicts:
    #	README.md

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyA3InEGaw3NDCXPDB1f4kaG8KYHrDr7sPk",
+    authDomain: "reacttodofirebase-5d158.firebaseapp.com",
+    databaseURL: "https://reacttodofirebase-5d158.firebaseio.com",
+    projectId: "reacttodofirebase-5d158",
+    storageBucket: "",
+    messagingSenderId: "136563403181"
+};
